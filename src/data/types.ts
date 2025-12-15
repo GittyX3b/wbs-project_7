66217f27a08cc8535f8bfe90de6dd22c9524b4d6
Dandy Nagel <github@dandynagel.com>
2025-12-15 11:14:48 +0100

    definition schema and type for artwork

--- src/data/types.ts
+++ src/data/types.ts
@@ -1,1 +1,10 @@
 import * as z from 'zod';
+
+const ArtworkSchema = z.object({
+    id: z.number(),
+    title: z.string(),
+    artist_title: z.string(),
+    image_id: z.number(),
+});
+
+type Artwork = z.infer<typeof ArtworkSchema>;
