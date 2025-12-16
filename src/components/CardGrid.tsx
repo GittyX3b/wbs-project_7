8ebebd65ea4f9cd1594e6a1bd759598a7f9623f8
Dandy Nagel <94107254+GittyX3b@users.noreply.github.com>
2025-12-16 15:56:48 +0100

    quickfix false loading state (#6)

--- src/components/CardGrid.tsx
+++ src/components/CardGrid.tsx
@@ -10,7 +10,7 @@ type CardGridProps = {
 const CardGrid = ({ loading, data }: CardGridProps) => {
     return (
         <>
-            {loading ? (
+            {!loading ? (
                 <div className='grid gap-14 p-10 sm:grid-cols-2 lg:grid-cols-3 lg:gap-4 lg:pl-20 xl:grid-cols-4'>
                     {data?.map((item) => (
                         <Card key={item.id} data={item} />
